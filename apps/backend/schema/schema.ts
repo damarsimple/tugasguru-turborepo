90f44503f87465b9da3b7663b576aa864168c8d2
Damar Albaribin <damaralbaribin@gmail.com>
2021-12-24 17:46:08 +0700

    123

--- apps/backend/schema/schema.ts
+++ apps/backend/schema/schema.ts
@@ -8,8 +8,8 @@ export const schema = makeSchema({
   types: { ...ModelTypes, ...MutationTypes, ...QueryTypes }
   ,
   outputs: {
-    typegen: join(__dirname, '..', 'nexus-typegen.ts'), // 2
-    schema: join(__dirname, '..', 'schema.graphql'), // 3
+    typegen: join(__dirname, '../', 'nexus-typegen.ts'), // 2
+    schema: join(__dirname, '../', 'schema.graphql'), // 3
   },
   contextType: {
     module: join(__dirname, '../api', 'context.ts'),
