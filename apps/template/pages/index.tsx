b6fbf69f7d599ec9dea82f6e364f76b5f7007556
Damar Albaribin <damaralbaribin@gmail.com>
2021-12-29 10:08:07 +0700

    feat any

--- apps/template/pages/index.tsx
+++ apps/template/pages/index.tsx
@@ -1,12 +1,6 @@
-import { AppProvider, Button } from "ui";
-import { useAuthStore } from "ui/stores/auth";
-import { useUserStore } from "ui/stores/user";
 
 export default function Docs() {
-  const { user } = useUserStore();
   return (
-    <AppProvider>
-      <Button>Hello {user.name}</Button>
-    </AppProvider>
+    <></>
   );
 }
